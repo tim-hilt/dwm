03a2b87b74c94411c6c9417620f49f9595c8e83f
Tim Hilt <timhilt@live.de>
2020-08-21 10:39:25 +0200

    Added directories to repo

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ -d ./src/ ]; then
+if [ -d ./src/ ]
+then
     rm -rf ./src/
 fi
 
 git clone https://git.suckless.org/dwm ./src/
-cp config.def.h ./src/
 cd ./src/
+
+for file in ../patches/*
+do
+    patch < $file
+done
+
+cp ../config.def.h .
 make
 sudo make install
