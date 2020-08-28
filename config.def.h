d2c93b40e04fda002c2a5c955c0325dceca27995
Tim Hilt <timhilt@live.de>
2020-08-28 16:05:44 +0200

    Added stuff to autostart

--- config.def.h
+++ config.def.h
@@ -25,9 +25,12 @@ static const char *colors[][3]      = {
 };
 
 static const char *const autostart[] = {
-	"hsetroot", "-cover", "../assets/background.jpg", NULL,
+	"hsetroot", "-cover", "/home/tim/git/dwm/assets/background.jpg", NULL,
 	"sh", "-c", "while :; do xsetroot -name \"$(date +\'%a %d. %b %Y, %R\')\"; sleep 60; done", NULL,
+	"sh", "-c", "/home/tim/git/dwm/assets/displayhandler", NULL,
 	"insync", "start", NULL,
+	"udiskie", NULL,
+	"dunst", NULL,
 	NULL /* terminate */
 };
 
