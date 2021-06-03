e1ac0b509b1828fa3fa1115c23f469dade60ed40
Tim Hilt <timhilt@live.de>
2021-06-03 11:21:53 +0200

    Switched from nix-shell to flake.nix

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+  let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShell.x86_64-linux = pkgs.mkShell { buildInputs = with pkgs; [
+      xorg.libX11.dev
+      xorg.libXft
+      xorg.libXinerama
+      gnumake
+      clang
+    ]; };
+  };
+}
